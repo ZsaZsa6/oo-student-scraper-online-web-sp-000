3f91f0ce7315937c415af6cf43c00a340ff750be
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-04 01:41:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
 
   def self.scrape_index_page(index_url)
-    # index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+  
     scraped_students = []
     index_page = Nokogiri::HTML(open(index_url))
     index_page.css("div.roster-cards-container").each do |profile|
