343f030e7e4d1d3ffb81ffff1407fe4af9bd5fb5
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-04 02:08:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each 
+    students_array.each do 
   end
 
   def add_student_attributes(attributes_hash)
