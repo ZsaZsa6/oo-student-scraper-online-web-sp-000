beb8eab15da3dad7452eb9aebb8e0b0ece03ab2d
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-04 02:16:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,9 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do | attributes, value |
       self.send("#{attribute}=", value)
-
   end
-
+  self
+  end
   def self.all
     @@all
   end
